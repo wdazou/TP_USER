2140d32012dfa79fc714dc49fe58567247c3d88a
wdazou <61542539+wdazou@users.noreply.github.com>
2020-09-19 15:59:51 +0200

    Update creat_user.sh

--- script/creat_user.sh
+++ script/creat_user.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/bash
 
-
+#liste les noms des utilisateurs 
 grep bash /etc/passwd | cut -f1 -d: > user.csv
 
 while read -r line
 do
+	#afficher seulement le nom d'utilasateur pour faciliter la comparaison
 	awk ‘{"file.csv"; print ($1)}’> new_file.csv
 
 done < file.csv > new_file.csv
-
+#comparaison avec le base donnée
 comm -3 user.csv new_file1.csv > new_user.csv
 
+#creation des utilisateur ou employer qui est pas dans le base de donnée
 for i in $( cat users.txt ); do
     useradd $i
     echo "user $i added successfully!"
